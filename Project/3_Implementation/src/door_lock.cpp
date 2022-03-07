6638f8dedfeeb3aad19639e2be80e54df73720cf
SudheeraD <87614111+SudheeraDasari@users.noreply.github.com>
2022-03-07 13:57:37 +0530

    Create door_lock.cpp

---
+++ Project/3_Implementation/src/door_lock.cpp
@@ -0,0 +1,174 @@
+#include "door_lock.h"
+void unlockdoor()
+{
+delay(900);
+ 
+lcd.setCursor(0,0);
+lcd.println(" ");
+lcd.setCursor(1,0);
+lcd.print("Access Granted");
+lcd.setCursor(4,1);
+lcd.println("WELCOME!!");
+lcd.setCursor(15,1);
+lcd.println(" ");
+lcd.setCursor(16,1);
+lcd.println(" ");
+lcd.setCursor(14,1);
+lcd.println(" ");
+lcd.setCursor(13,1);
+lcd.println(" ");
+
+for(pos = 180; pos>=0; pos-=5) // open the door
+{
+myservo.write(pos); 
+delay(5); 
+}
+delay(2000);
+ 
+ 
+ 
+delay(1000);
+counterbeep();
+ 
+delay(1000);
+ 
+for(pos = 0; pos <= 180; pos +=5) // close the door
+{ // in steps of 1 degree
+myservo.write(pos); 
+delay(15);
+ 
+ 
+currentposition=0;
+ 
+lcd.clear();
+displayscreen();
+ 
+}
+}
+ 
+//--------------------Function 2- Wrong code--------------//
+ 
+void incorrect()
+{
+delay(500);
+lcd.clear();
+lcd.setCursor(1,0);
+lcd.print("CODE");
+lcd.setCursor(6,0);
+lcd.print("INCORRECT");
+lcd.setCursor(15,1);
+lcd.println(" ");
+lcd.setCursor(4,1);
+lcd.println("GET AWAY!!!");
+ 
+lcd.setCursor(13,1);
+lcd.println(" ");
+Serial.println("CODE INCORRECT YOU ARE UNAUTHORIZED");
+delay(3000);
+lcd.clear();
+displayscreen();
+}
+//-------Function 3 - CLEAR THE SCREEN--------------------/
+void clearscreen()
+{
+lcd.setCursor(0,0);
+lcd.println(" ");
+lcd.setCursor(0,1);
+lcd.println(" ");
+lcd.setCursor(0,2);
+lcd.println(" ");
+lcd.setCursor(0,3);
+lcd.println(" ");
+}
+
+//------------Function 4 - DISPLAY FUNCTION--------------------//
+void displayscreen()
+{
+ 
+lcd.setCursor(0,0);
+lcd.println("*ENTER THE CODE*");
+lcd.setCursor(1 ,1);
+ 
+lcd.println("TO OPEN DOOR!!");
+}
+
+
+ 
+//--------------Function 5 - Count down------------------//
+void counterbeep()
+{
+delay(1200);
+lcd.clear();
+ 
+lcd.setCursor(2,15);
+lcd.println(" ");
+lcd.setCursor(2,14);
+lcd.println(" ");
+lcd.setCursor(2,0);
+delay(200);
+lcd.println("GET IN WITHIN:::");
+ 
+lcd.setCursor(4,1);
+lcd.print("5");
+delay(200);
+lcd.clear();
+lcd.setCursor(2,0);
+lcd.println("GET IN WITHIN:");
+delay(1000);
+lcd.setCursor(2,0);
+lcd.println("GET IN WITHIN:");
+lcd.setCursor(4,1); //2
+lcd.print("4");
+delay(100);
+lcd.clear();
+lcd.setCursor(2,0);
+lcd.println("GET IN WITHIN:");
+delay(1000);
+
+lcd.setCursor(2,0);
+lcd.println("GET IN WITHIN:");
+lcd.setCursor(4,1); 
+lcd.print("3");
+delay(100);
+lcd.clear();
+lcd.setCursor(2,0);
+lcd.println("GET IN WITHIN:");
+delay(1000);
+
+lcd.setCursor(2,0);
+lcd.println("GET IN WITHIN:");
+lcd.setCursor(4,1); 
+lcd.print("2");
+delay(100);
+lcd.clear();
+lcd.setCursor(2,0);
+lcd.println("GET IN WITHIN:");
+delay(1000);
+
+lcd.setCursor(4,1);
+lcd.print("1");
+delay(100);
+lcd.clear();
+lcd.setCursor(2,0);
+lcd.println("GET IN WITHIN::");
+
+delay(1000);
+delay(40);
+lcd.clear();
+lcd.setCursor(2,0);
+lcd.print("RE-LOCKING");
+delay(500);
+lcd.setCursor(12,0);
+lcd.print(".");
+delay(500);
+lcd.setCursor(13,0);
+lcd.print(".");
+delay(500);
+lcd.setCursor(14,0);
+lcd.print(".");
+delay(400);
+lcd.clear();
+lcd.setCursor(4,0);
+lcd.print("LOCKED!");
+delay(440);
+}
