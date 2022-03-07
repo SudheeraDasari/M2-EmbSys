ee33390953106886a70d63bae6e49a31a1af00d1
SudheeraD <87614111+SudheeraDasari@users.noreply.github.com>
2022-03-07 13:50:40 +0530

    Update door_lock.h

--- Project/3_Implementation/inc/door_lock.h
+++ Project/3_Implementation/inc/door_lock.h
@@ -1,1 +1,34 @@
+**
+ * This is header file
+ */
 
+
+
+
+#ifndef DOOR_LOCK_H
+#define DOOR_LOCK_H
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+#include <Servo.h>
+#include <Keypad.h>
+void unlockdoor();
+/**
+ * Function 1- OPEN THE DOOR
+ */
+void incorrect();
+/**
+ * Function 2- Wrong code
+ */
+void clearscreen();
+/**
+ * Function 3 - CLEAR THE SCREEN
+ */
+void displayscreen();
+/**
+ * Function 4 - DISPLAY FUNCTION
+ */
+void counterbeep();
+/**
+ * Function 5 - Count down
+ */
+#endif
